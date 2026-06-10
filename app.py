6d2964680f859c92b7604df38fbe0516c87a77df
yaya588 <ingineeryaya@gmail.com>
2026-06-10 23:56:26 +0200

    version 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def accueil():
     return jsonify({
-        "message": "Bonjour depuis mon API CI/CD"
+        "message": "version 2 du service"
     })
 
 if __name__ == "__main__":
