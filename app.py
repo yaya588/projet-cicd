c617b722e568db192239675597882c3a08af0adc
yaya588 <ingineeryaya@gmail.com>
2026-06-11 10:00:27 +0200

    info demo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def accueil():
     return jsonify({
-        "message": "version 2 du service"
+        "message": "l3 info enastic"
     })
 
 if __name__ == "__main__":
